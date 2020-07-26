a4a1ccad929465f6800b72cfda0e90fee3468ab3
Elvis <elvishuges@hotmail.com>
2020-07-25 21:51:09 -0300

    add dao

--- src/daos/userDao.js
+++ src/daos/userDao.js
@@ -6,19 +6,17 @@ exports.login = function (username, password) {
         console.log("Username: " + username);
         console.log("Password: " + password);
 
-        var sql = "select * from medico where email = '" + username + "' and senha = '" + password + "'";
+        var sql = "select * from user where email = '" + username + "' and senha = '" + password + "'";
         con.query(sql, function (err, result) {
             console.log("ERRO: " + err);
-            //console.log("RESULTADO: |" + result[0] + "|");
             if (result[0] != undefined) {
                 console.log("Aqui!");
-                result[0].funcao = "medico"
+                result[0].funcao = "user"
                 fulfill(result[0]);
             } else {
                 var sql = "select * from atendente where email = '" + username + "' and senha = '" + password + "'";
                 con.query(sql, function (err, result) {
                     console.log("ERRO: " + err);
-                    //console.log("RESULTADO: |" + result[0] + "|");
                     if (result[0] != undefined) {
                         console.log("Aqui2!");
 
